3799288a91d4e055aebc87a7f1a03242f75bf73d
Jousephine Pardo <j.pardo@aiep.nnw>
2021-11-15 22:52:58 -0300

    creando

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+    def new
+        @article = Article.new
+        @article.title = 'Demo'
+    end
+
+    def create 
+        @article = Article.create(title: params[:article][:title])
+    end
+
+end
