e830c8f106266c64dbc26d449215659d6c5ac47b
Jousephine Pardo <j.pardo@aiep.nnw>
2021-11-15 23:40:11 -0300

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
+    
+    def show
+        @article = Article.find(2)
+    end
+    
     def new
         @article = Article.new
         @article.title = 'Demo'
     end
 
     def create 
-        @article = Article.create(title: params[:article][:title])
+        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+        render json: @article
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  get '/bienvenida' , to: 'home#index'
 
  get "articles/new", to: "articles#new"
+ get "articles/2", to: "articles#show"
  post "articles", to:"articles#create"
 end
