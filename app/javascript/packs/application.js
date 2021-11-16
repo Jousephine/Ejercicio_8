99dae8bfb5b41de2fb0a67eee03d40b073b50578
Jousephine Pardo <j.pardo@aiep.nnw>
2021-11-16 19:54:56 -0300

    devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "css/styles"
+
+import 'bootstrap'
+import 'css/styles'
 
 Rails.start()
 Turbolinks.start()
