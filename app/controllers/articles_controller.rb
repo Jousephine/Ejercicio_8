4081476b8b59ab454a4f694341fd975aef0ed537
Jousephine Pardo <j.pardo@aiep.nnw>
2021-11-16 01:35:40 -0300

    navegador

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,27 @@
 class ArticlesController < ApplicationController
-    
-    def show
-        @article = Article.find(2)
+    before_action :find_article, except: [:new, :create, :index]
+
+    def index
+        @articles = Article.all
     end
-    
+
+
+    def show
+     
+    end
+
+    def edit
+   
+    end
+
+    def update
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        
+        redirect_to @article
+    end
+
     def new
-        @article = Article.new
-        @article.title = 'Demo'
+    
     end
 
     def create 
@@ -14,4 +29,12 @@ class ArticlesController < ApplicationController
         render json: @article
     end
 
+    def destroy
+        @article.destroy
+        redirect_to root_path
+    end
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
  get '/bienvenida' , to: 'home#index'
+ 
+ root to: "home#index"
 
- get "articles/new", to: "articles#new"
- get "articles/2", to: "articles#show"
+ get "articles", to: "articles#index"
+ get "articles/new", to: "articles#new", as: :new_articles
+ get "articles/:id", to: "articles#show"
+ get "articles/:id/edit", to: "articles#edit"
+
+ patch "/articles/:id",to: "articles#update", as: :article
  post "articles", to:"articles#create"
+ delete "/articles/:id", to: "articles#destroy"
+
 end
